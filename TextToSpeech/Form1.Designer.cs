e0318effa577bc98920ecde267d5f229e1cb396d
MERT\Mert <mertozturk1903@gmail.com>
2017-10-30 13:47:12 +0300

    10/30/2017

---
+++ TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SubTitle = new System.Windows.Forms.Label();
+            this.SubTitleTimer = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SubTitle
+            // 
+            this.SubTitle.AutoSize = true;
+            this.SubTitle.BackColor = System.Drawing.Color.Transparent;
+            this.SubTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SubTitle.Font = new System.Drawing.Font("Comic Sans MS", 14F);
+            this.SubTitle.Location = new System.Drawing.Point(3, 0);
+            this.SubTitle.Name = "SubTitle";
+            this.SubTitle.Size = new System.Drawing.Size(1114, 26);
+            this.SubTitle.TabIndex = 0;
+            this.SubTitle.Text = "label1";
+            this.SubTitle.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // SubTitleTimer
+            // 
+            this.SubTitleTimer.Interval = 250;
+            this.SubTitleTimer.Tick += new System.EventHandler(this.SubTitleTimer_Tick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.SubTitle, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 469);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1120, 30);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = global::TextToSpeech.Properties.Resources.jarvis;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1120, 499);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label SubTitle;
+        private System.Windows.Forms.Timer SubTitleTimer;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        bool cikisDurum = false;
+        DateTime time = new DateTime();
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        RegistryKey key = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+
+        string[] konusulan;
+        int kelimesayaci = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            key.SetValue("DisableTaskMgr", 0);
+            time = DateTime.Now;
+            SubTitle.Text = "";
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!cikisDurum)
+            {
+                e.Cancel = true;
+                Konus("You can not close me.");
+            }
+            else
+                e.Cancel = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!cikisDurum)
+            {
+                SetForegroundWindow(this.Handle);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cikisDurum = true;
+            Application.Exit();
+        }
+
+        SpeechSynthesizer sSynth = new SpeechSynthesizer();
+        PromptBuilder pBuilder = new PromptBuilder();
+        SpeechRecognitionEngine sRecognize = new SpeechRecognitionEngine();
+
+        void Konus(string metin)
+        {
+            SubTitle.Text = metin;
+            sSynth.SelectVoice("Microsoft David Desktop");
+            pBuilder.ClearContent();
+            pBuilder.AppendText(metin);
+            sSynth.Speak(pBuilder);
+            SubTitle.Text = "";
+        }
+
+        void AltYazi(string metin)
+        {
+            SubTitle.Visible = true;
+            konusulan = metin.Split();
+
+            foreach (var item in konusulan)
+            {
+                kelimesayaci += 1;
+            }
+            SubTitleTimer.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Konus("Welcome back sir!");
+            Konus("open");
+            // Konus("How are you?");
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            TimeSpan fark = DateTime.Now - time;
+            if (fark.Minutes > 10)
+                Konus("Welcome Back Sir");
+            time = DateTime.Now;
+
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!cikisDurum)
+                    cikisDurum = true;
+                else
+                    cikisDurum = false;
+            }
+            
+
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                if (!cikisDurum)
+                {
+                    Konus("You can not close me.");
+                }
+
+            }
+            if (e.KeyCode == Keys.T)
+            {
+                Konus("I am listening sir!");
+                Choices sList = new Choices();
+                sList.Add(new string[] { "start chrome", "hello", "test", "it works", "how", "are", "you", "today", "i", "am", "fine", "exit", "so" });
+                Grammar gr = new Grammar(new GrammarBuilder(sList));
+                try
+                {
+                    sRecognize.RequestRecognizerUpdate();
+                    sRecognize.LoadGrammar(gr);
+                    sRecognize.SpeechRecognized += sRecognize_SpeechRecognized;
+                    sRecognize.SetInputToDefaultAudioDevice();
+                    sRecognize.RecognizeAsync(RecognizeMode.Multiple);
+
+                }
+                catch (Exception)
+                {
+
+                    return;
+                }
+            }
+        }
+        public string sonKelime = "";
+        private void sRecognize_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text == "exit")
+            {
+                cikisDurum = true;
+                Application.Exit();
+            }
+            else if (e.Result.Text == "start chrome"&& sonKelime!="start chrome")
+            {
+                //MessageBox.Show("ok");
+                System.Diagnostics.Process.Start("chrome.exe", "http://www.google.com.tr");
+
+            }  
+            //MessageBox.Show("speech recognized:" + e.Result.Text.ToString());
+            sonKelime = e.Result.Text.ToString();
+        }
+        string[] elemansil(string[] dizi)
+        {
+            string[] dizi2 = new string[dizi.Length - 1];
+            int sayac = dizi.Length;
+            for (int i = 1; i < sayac; i++)
+            {
+                dizi2[i - 1] = dizi[i];
+            }
+            return dizi2;
+        }
+        private void SubTitleTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
